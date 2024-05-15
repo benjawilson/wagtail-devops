687e93a47149aa813c812b62fbce9ecac1ba869c
Ben Wilson <ben@benwilson.us>
2024-05-14 18:27:31 -0700

    Edit production settings

--- wagops/wagops/settings/production.py
+++ wagops/wagops/settings/production.py
@@ -3,10 +3,10 @@ from .base import *
 STATIC_URL = "static/"
 DEBUG = False
 SECRET_KEY = "5m$x8nxvk$!3c9m^c!o8*a-*4fn#ox@ag2vb45$9sgrf25of)1"
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 #SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
-#SECURE_SSL_REDIRECT = True
+#SECURE_SSL_REDIRECT = True 
 #SECURE_HSTS_SECONDS = 0
 
 try:
