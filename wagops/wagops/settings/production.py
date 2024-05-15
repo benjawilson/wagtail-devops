e9eacb752b8491a656bf0ddf55bdcaa0a4cb1c72
Ben Wilson <ben@benwilson.us>
2024-05-14 20:21:00 -0700

    Add more specific trusted origin

--- wagops/wagops/settings/production.py
+++ wagops/wagops/settings/production.py
@@ -6,7 +6,7 @@ SECRET_KEY = "5m$x8nxvk$!3c9m^c!o8*a-*4fn#ox@ag2vb45$9sgrf25of)1"
 ALLOWED_HOSTS = ['*']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ["https://*.ondigitalocean.app"]
 #SECURE_SSL_REDIRECT = True 
 #SECURE_HSTS_SECONDS = 0
 
